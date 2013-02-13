1f187bfdd572463b73c3bc56bbfe153f6f5bc143
Moreno Ambrosin <morambro@gmail.com>
2013-02-13 16:23:26 +0100

    Correction

--- Train-Track_Multiple_Access/railway-train.adb
+++ Train-Track_Multiple_Access/railway-train.adb
@@ -12,7 +12,6 @@ package body Railway.Train is
 				accept Initialize(Descr : Train_Descriptor) do
 					Current_Descriptor := Descr;
 				end;
-			or
 			
 				Max_Speed := Current_Descriptor.Speed;
 			
