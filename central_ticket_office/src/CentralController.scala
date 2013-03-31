ee77f518c78752b67a55d7ff6b20b14b04d74ee6
Moreno Ambrosin <morambro@gmail.com>
2013-03-31 12:14:34 +0200

    Added json loading to central_ticket_office server

---
+++ central_ticket_office/src/CentralController.scala
@@ -0,0 +1,148 @@
+import scala.actors._
+import com.inspirel.yami._
+
+case class Event (description : String) 
+
+object Event {
+	def createFromJson (Json_String : String) : Event = Event(Json_String)
+}
+
+class Publisher(val address : String) extends Actor {
+	
+	val publisherValue	= new ValuePublisher
+	val publisherAgent	= new Agent
+	
+	// Setting address
+	publisherAgent.addListener(address)
+	
+	publisherAgent.registerValuePublisher("events",publisherValue)
+	
+
+	def controller_loop() {
+		react {
+			case Event(d) => {
+				val content = new Parameters
+				println ("Publishing event " + d)
+				content.setString("event",d)
+				publisherValue.publish(content)
+				controller_loop
+			}
+			case "stop" => println("controller Stops")
+		}
+	}
+
+	def act() {
+		controller_loop
+	}
+	
+}
+
+/*class Subscriber(ID : Int) extends Actor with IncomingMessageCallback{
+	
+	val subscriberAgent = new Agent
+
+	def subscriber_loop {
+		react {
+			case "init" => {
+				println ("init!")
+				subscriberAgent.registerObject("update_handler", this);
+				val params : Parameters = new Parameters
+				params.setString("destination_object", "update_handler");
+				subscriberAgent.sendOneWay("tcp://localhost:2222","events", "subscribe", params);
+				subscriber_loop
+			}
+		}		
+	}
+
+	def act() {
+		subscriber_loop
+	}
+	
+	def call (incomingMessage : IncomingMessage) {
+		try {
+			val content = incomingMessage.getParameters()
+
+		    val event = content.getString("event")
+		    
+		    println("Subscriber " + ID + " Received event : " + event)
+		}catch{
+			case e => e.printStackTrace 
+		}
+	}
+	
+}*/
+
+
+class Receiver(val controller : Publisher) extends IncomingMessageCallback {
+	
+	var serverAgent = new Agent
+	
+	def addHandler (address : String) {
+		val add = serverAgent.addListener(address);
+		println("Central Controller listening on address " + address)
+		serverAgent.registerObject("central_controller",this)
+	}
+	
+	def call(im : IncomingMessage) {
+		im.getMessageName match {
+			case "event" => {
+				controller ! Event.createFromJson(im.getParameters.getString("content"))
+			}
+			case _ => {
+				print("Invalid event")
+			}
+		}
+	}
+	
+	def close {
+		serverAgent.close
+	}
+}
+
+
+object ControllerMain extends App {
+	
+	var controller 	: Publisher = null
+	var receiver 	: Receiver = null
+	
+	def waitExit {
+		readLine() match {
+			case "q" | "Q" => {
+				controller ! "stop"
+				receiver.close
+				println ("Bye!")
+			}
+			case a:String => {
+				controller ! Event(a)
+				waitExit
+			}
+		}
+	}
+	
+	override def main(args : Array[String]) {
+		
+		if (args.length < 1) {
+			println ("ERROR: Controller tcp address must be specified");
+			return;
+		}
+		controller = new Publisher("tcp://localhost:2222")
+		controller.start		
+		receiver = new Receiver(controller)
+		receiver.addHandler(args(0));
+		
+		/*
+		val s = new Subscriber(1)
+		s.start
+		s ! "init"
+		
+		val s2 = new Subscriber(2)
+		s2.start
+		s2 ! "init"
+		*/
+		
+		waitExit
+		
+	} 
+	
+	
+}
---
+++ central_ticket_office/src/ticket_office.scala
@@ -0,0 +1,139 @@
+import scala.actors._
+import com.inspirel.yami._
+import scala.util.parsing.json._
+
+case class Stop()
+case class Resolve(from:String,to:String)
+
+
+class RegionalStation(parId : String, parAddress : String) {
+	
+	val id = parId;
+	
+	val address = parAddress;
+	
+}
+
+object PathResolver {
+	
+	/**
+	 * Loads the json file containg the location of regional ticket offices
+	 *
+	 * @return A list of couples (name,address)
+	 */ 
+	def load() : List[(String,String)] = {
+		
+		val json = scala.io.Source.fromFile("../env.json").mkString
+
+		val json_elements = JSON.parseFull(json)
+	
+		var res : List[(String,String)] = List()
+
+		json_elements match {
+			case Some(el) => el match {
+				case map : Map[String,List[_]] => {
+					map.get("regional_offices") match {
+						case Some(v : List[Map[String,String]]) 	=> {
+							v.foreach( office => {
+								val name : String = office("name")
+								val address : String = office("address")
+								res = (name,address) :: res
+							})
+						}
+						case Some(_) 	=> println("Error") 
+						case None 		=> println("Error")
+					}
+				}
+				case _ => println("ciao")
+			}
+			case None 	=> println("Parsing Error")
+		}
+		
+		res	
+	}
+	
+	def ask(office : (String,String), station : String) : (String,String) = {
+		if (office._1 == "office_2") return office
+		return null
+	}
+	
+}
+
+class PathResolver extends Actor {
+	
+	// The list of couples <regional office,address> 
+	var regional_offices	: List[(String,String)] = PathResolver.load();
+	// Map containg for each region the list of regions to go through to reach it 
+	var regions_map 		: Map[String,List[(String,String)]] = null
+
+	def resolverLoop() {
+		react {
+			case Resolve(from,to) => {
+				println("I have to resolve form " + from + " to " + to)
+				
+				var region : (String,String) = null;
+				for {
+					o <- regional_offices;
+					if(region == null)   
+				}{
+					region = PathResolver.ask(o,to)
+				}
+				
+				println("Station " + to + " is in Region " + region._1)
+				
+				resolverLoop
+			}
+			case Stop() => {
+				println("Tearing down Path resolver")
+			}
+		}
+	}
+
+	def act() = resolverLoop()
+	
+
+}
+
+class RequestReceiver(address : String, resolver : Actor) extends Actor with IncomingMessageCallback{
+	
+	val serverAgent : Agent = new Agent;
+	val resolvedAddress 	= serverAgent.addListener(address);
+	serverAgent.registerObject("cantral_ticket_server", this);
+	
+	def receiverLoop() {
+		react {
+			case Stop()	=> println("Bye") 
+		}
+	}
+		
+	
+	def act() = receiverLoop()
+	
+	def call(im : IncomingMessage) {
+		im.getMessageName match {
+		
+			case "resolve"	=>	{
+				
+				val start 		= im.getParameters.getString("start")
+				val destination = im.getParameters.getString("destination")			
+				
+				resolver ! Resolve(start,destination)
+				
+			}
+			
+		}
+	}
+}
+
+object Main extends App {
+
+	val resolver = new PathResolver;resolver.start
+	
+	val receiver = new RequestReceiver("tcp://localhost:9999",resolver)
+	receiver.start
+	
+	resolver ! Resolve("f","t")
+	
+}
+
+
