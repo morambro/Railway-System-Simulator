2fec56b9b4c39a78b54591106cf0a4537d58aef3
Moreno Ambrosin <morambro@gmail.com>
2013-04-01 02:26:45 +0200

    Name server has changed, now it keeps a map (station,node_name) => address

--- name_server/src/NameServer.scala
+++ name_server/src/NameServer.scala
@@ -10,7 +10,7 @@ package it.name_server {
 	class ServerAgent extends Actor with IncomingMessageCallback{
 	
 		var serverAgent : Agent = null;
-		var addresses	: Map[String,String] = Map();
+		var addresses	: Map[(String,String),String] = Map();
 	
 		def act() {
 			my_loop
@@ -34,13 +34,16 @@ package it.name_server {
 	
 		def call(im : IncomingMessage) {
 		
+			println("Received Message "+im.getMessageName)
+		
 			im.getMessageName match {
 			
 				case "add" => {
-					val name 	= im.getParameters.getString("name")
-					val address = im.getParameters.getString("address")
+					val station		= im.getParameters.getString("station")
+					val node_name 	= im.getParameters.getString("node_name") 
+					val address 	= im.getParameters.getString("address")
 		
-					addresses += (name -> address)
+					addresses += ((station,node_name) -> address)
 		
 					println("Total entries : ")
 					for( key <- addresses.keys )
@@ -52,22 +55,23 @@ package it.name_server {
 			
 					val replyParams : Parameters = new Parameters;
 
-					replyParams.setString("address","ok");
+					replyParams.setString("response","ok");
 			
 					im.reply(replyParams)
 				}
 			
 				case "get" => {
-					val key = im.getParameters.getString("name")
+					val key 	= (im.getParameters.getString("station"),im.getParameters.getString("node_name"))
+					
 					val replyParams : Parameters = new Parameters;
 					addresses get key match {
 						case Some(x) => {
 							println("Found entry : " + key + "  =>  " + x)
-							replyParams.setString("address",x);
+							replyParams.setString("response",x);
 						}
 						case None => {
 							println("Entry " + key + " not found")
-							replyParams.setString("address","_");
+							replyParams.setString("response","_");
 						}
 					}
 				
