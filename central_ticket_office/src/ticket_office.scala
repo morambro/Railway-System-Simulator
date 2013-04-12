281567bdfa8586491bc661d04bdb163a407d176b
Moreno Ambrosin <morambro@gmail.com>
2013-04-12 17:01:51 +0200

    Improved Json parsing using json-smart library instead of native scala library. Improved also error handling

--- central_ticket_office/src/ticket_office.scala
+++ central_ticket_office/src/ticket_office.scala
@@ -1,20 +1,11 @@
 import scala.actors._
 import com.inspirel.yami._
-import scala.util.parsing.json._
+import net.minidev.json._
 
 case class Stop()
 case class Resolve(startNode:String,from:String,to:String)
 case class Path(path : String)
 case class Error(message:String)
-
-
-class RegionalStation(parId : String, parAddress : String) {
-	
-	val id = parId;
-	
-	val address = parAddress;
-	
-}
 
 object PathResolver {
 	
@@ -31,28 +22,23 @@ object PathResolver {
 		
 		//val json = scala.io.Source.fromFile(fileName).mkString
 
-		val json_elements = JSON.parseFull(json)
-	
 		var res : Map[String,String] = Map()
 
-		json_elements match {
-			case Some(el) => el match {
-				case map : Map[String,List[_]] => {
-					map.get("nodes") match {
-						case Some(v : List[Map[String,String]]) 	=> {
-							v.foreach( office => {
-								val name : String = office("name")
-								val address : String = office("address")
+
+		JSONValue.parseStrict(json) match {
+			case o : JSONObject => o.get("nodes") match {
+				case arr : net.minidev.json.JSONArray => {
+					for(i <- 0 until arr.size) {
+						arr get (i) match {
+							case el : net.minidev.json.JSONObject => {
+								val name 	: String = el.get("name").toString
+								val address : String = el.get("address").toString
 								res = res + Tuple2(name,address)
-							})
+							}
 						}
-						case Some(_) 	=> println("Error") 
-						case None 		=> println("Error")
 					}
 				}
-				case _ => println("ciao")
 			}
-			case None 	=> println("Parsing Error")
 		}
 		
 		res	
@@ -64,55 +50,34 @@ object PathResolver {
 		
 		var res : Map[(String,String),List[(String,String)]] = Map()
 		
-		JSON.parseFull(json) match {
-			case Some(map) => map match {
-				case m : Map[String,_] => {
-					m get "links" match {
-						case Some(el) => el match {
-							case l : List[_] => {
-								l.foreach( elem => elem match {
-									case m : Map[String,_] => {
-										var reg1 : String = ""
-										var reg2 : String = ""
-										var path : List[(String,String)] = List()
-										m get "region1" match {
-											case Some(r1) => r1 match {
-												case r1s : String => reg1 = r1s
-											}
-											case None => println("Parsing ERROR!")
-										}
-										m get "region2" match {
-											case Some(r2) => r2 match {
-												case r2s : String => reg2 = r2s
-											}
-											case None => println("Parsing ERROR!")
-										}
-										
-										m get "paths" match {
-											case Some(p) => p match {
-												case l : List[_] => {
-													l.foreach (item => item match {
-														case m : Map[String,String] => {
-															path = (m("node"),m("station")) :: path
-														} 
-													})
+		JSONValue.parseStrict(json) match {
+			case o : JSONObject => o.get("links") match {
+				case links : JSONArray => {
+					for (i <- 0 until links.size) {
+						links.get(i) match {
+							case el : JSONObject => {
+								val reg1 : String = el.get("region1").toString
+								val reg2 : String = el.get("region2").toString
+								var pathsList : List[(String,String)] = List()
+								el.get("paths") match {
+									case paths : JSONArray => {
+										for (j <- 0 until paths.size()) {
+											paths.get(j) match {
+												case item : JSONObject => {
+													pathsList = pathsList :+ Tuple2(
+														item.get("node").toString,
+														item.get("station").toString)
 												}
 											}
-											case None => println("Parsing ERROR!")
 										}
-										 
-										res = res + Tuple2((reg1,reg2),path)
 									}
-								})
+								}
+								res = res + Tuple2(Tuple2(reg1,reg2),pathsList)
 							}
 						}
-						case None => println("Parsing ERROR!")
-					
 					}
 				}
-				case _ => println("Parsing ERROR!")
 			}
-			case None => println("Parsing ERROR!")			
 		}
 		
 		res
@@ -162,7 +127,9 @@ class PathResolver(fileName : String) extends Actor {
 
 	/**
 	 *	For each couple (Node_Name,Address) search for destination <To>. 
-	 *  	@return the name of the node containg the station <To>
+	 *  
+	 *	@return the name of the node containg the station <To>
+	 *
 	 **/
 	def ask(to : String) : String = {
 		nodesAddresses.keys.foreach(k => {
@@ -171,35 +138,42 @@ class PathResolver(fileName : String) extends Actor {
 			
 			p.setString("station",to) 
 			
-			val message : OutgoingMessage = agent.send(
-				nodesAddresses(k),
-				"message_handler", 
-				"is_present", 
-				p);
-		    
-			message.waitForCompletion
+			try{
+				val message : OutgoingMessage = agent.send(
+					nodesAddresses(k),
+					"message_handler", 
+					"is_present", 
+					p);
+				message.waitForCompletion
 			
-			message.getState match {
-				case OutgoingMessage.MessageState.REPLIED => {
+				message.getState match {
+					case OutgoingMessage.MessageState.REPLIED => {
 			
-					val reply = message.getReply();
+						val reply = message.getReply();
 			
-					val found = reply.getString("result")
+						val found = reply.getString("result")
 		
-					if (found == "TRUE") return k
-				} 
-				case OutgoingMessage.MessageState.REJECTED => {
-					println("The message for node " +k+ " has been rejected: " + message.getExceptionMsg)
+						if (found == "TRUE") return k
+					} 
+					case OutgoingMessage.MessageState.REJECTED => {
+						println("The message for node " +k+ " has been rejected: " + message.getExceptionMsg)
+					}
+					case _ => {
+						println("The message for node " +k+ " has been abandoned.")
+					}
 				}
-				case _ => {
-					println("The message for node " +k+ " has been abandoned.")
+			} catch {
+				case e : com.inspirel.yami.YAMIIOException => {
+					println("ERRORE: Connessione rifiutata all'indirizzo " + nodesAddresses(k))
 				}
-			}
+			} 
 		})
 		null
 	}
 	
-	
+	/**
+	 * Ask a node to create a ticket from [form] to [to]
+	 **/
 	def ask (node:String,from:String,to:String) {
 
 		val p = new Parameters
@@ -247,29 +221,36 @@ class PathResolver(fileName : String) extends Actor {
 					
 					val region = ask(to)
 				
-					println("Station " + to + " is in Region " + region)
+					if (region == null) {
+						reply  {
+							Error("ERROR : No region found for station " + to)
+						}
+						// ...
+					} else {
+						println("Station " + to + " is in Region " + region)
 				
-					// Now we have startNode and region to reach. We have to find
-					// the gateways to cross to build a path					
-					regionsMap get (startNode,region) match {
-						case Some(item) => {
-							var node = startNode
-							var f = from
-							item match {
-								case l : List[_] => {
-									l.foreach(c => {
-										println("Search from "+f+" to "+c._2+" region " +  node)
-										ask(node,f,c._2)
-										node = c._1
-										f = c._2
-									})
-									println("Search from "+f+" to "+to+" region " +  node)
-									ask(node,f,to)
+						// Now we have startNode and region to reach. We have to find
+						// the gateways to cross to build a path					
+						regionsMap get (startNode,region) match {
+							case Some(item) => {
+								var node = startNode
+								var f = from
+								item match {
+									case l : List[_] => {
+										l.foreach(c => {
+											println("Search from "+f+" to "+c._2+" region " +  node)
+											ask(node,f,c._2)
+											node = c._1
+											f = c._2
+										})
+										println("Search from "+f+" to "+to+" region " +  node)
+										ask(node,f,to)
+									}
 								}
 							}
-						}
-						case None => reply {
-							Error("No path to reach " + region + " from " + startNode)
+							case None => reply {
+								Error("No path to reach " + region + " from " + startNode)
+							}
 						}
 					}
 				}
@@ -294,7 +275,6 @@ class RequestReceiver(address : String, resolver : Actor) extends Actor with Inc
 	
 	serverAgent.registerObject("cantral_ticket_server", this);
 	
-		
 	
 	def receiverLoop() {
 		react {
@@ -310,7 +290,6 @@ class RequestReceiver(address : String, resolver : Actor) extends Actor with Inc
 		im.getMessageName match {
 		
 			case "resolve"	=>	{
-				
 				
 				val start 		= im.getParameters.getString("start")
 				val startNode 	= im.getParameters.getString("start_node")
@@ -334,12 +313,16 @@ object Main extends App {
 		}
 		val resolver = new PathResolver(argv(0))
 		resolver.start
-		
-		resolver ! Resolve("Node_1","1","H")
-		
+
 		PathResolver.NAME_SERVER_ADDRESS = argv(1)
-	
-	
+			
+				
+		resolver !? Resolve("Node_1","1","H") match {
+			case Error(msg) => println(msg)
+			case _ => println("ok")
+		}
+		
+
 		val receiver = new RequestReceiver("tcp://localhost:9999",resolver)
 		receiver.start
 	}
